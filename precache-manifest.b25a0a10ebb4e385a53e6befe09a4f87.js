18b9aac96878a5c311512056ec6a769792abe6a5
NikitaKS <pancxxx2@gmail.com>
2019-11-11 12:56:26 +0100

    Updates

---
+++ precache-manifest.b25a0a10ebb4e385a53e6befe09a4f87.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb0947403811be97ce0e1a8a6eae4342",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "7be574fc512a91720b37",
+    "url": "/Portfolio/static/css/2.117ce29f.chunk.css"
+  },
+  {
+    "revision": "ae7721122f8169d01dc9",
+    "url": "/Portfolio/static/css/main.162e5fea.chunk.css"
+  },
+  {
+    "revision": "7be574fc512a91720b37",
+    "url": "/Portfolio/static/js/2.b1235074.chunk.js"
+  },
+  {
+    "revision": "ae7721122f8169d01dc9",
+    "url": "/Portfolio/static/js/main.0635b383.chunk.js"
+  },
+  {
+    "revision": "63fe68486ecb758e5a56",
+    "url": "/Portfolio/static/js/runtime-main.b0957d76.js"
+  },
+  {
+    "revision": "073736d35d7a45c4dfdc7f590594b41f",
+    "url": "/Portfolio/static/media/hkgrotesk-bold-webfont.073736d3.woff"
+  },
+  {
+    "revision": "5f2ca98a5ced46fb06dbb56203b7b6b5",
+    "url": "/Portfolio/static/media/hkgrotesk-bold-webfont.5f2ca98a.woff2"
+  },
+  {
+    "revision": "626bc65568d88919db5422680f475f91",
+    "url": "/Portfolio/static/media/hkgrotesk-italic-webfont.626bc655.woff2"
+  },
+  {
+    "revision": "a3101a6313004136505cd5f57a56aa8a",
+    "url": "/Portfolio/static/media/hkgrotesk-italic-webfont.a3101a63.woff"
+  },
+  {
+    "revision": "0296e587f17b62910aec82e853913907",
+    "url": "/Portfolio/static/media/hkgrotesk-light-webfont.0296e587.woff"
+  },
+  {
+    "revision": "78335b6e3f422871af3397a4203b4c46",
+    "url": "/Portfolio/static/media/hkgrotesk-light-webfont.78335b6e.woff2"
+  },
+  {
+    "revision": "32bff7a50bbb72371380ff33224b71fe",
+    "url": "/Portfolio/static/media/hkgrotesk-medium-webfont.32bff7a5.woff2"
+  },
+  {
+    "revision": "7359ebc9067adf876150d9d6796b9541",
+    "url": "/Portfolio/static/media/hkgrotesk-medium-webfont.7359ebc9.woff"
+  },
+  {
+    "revision": "585acda102e5309f82d1b677e9de3dcc",
+    "url": "/Portfolio/static/media/hkgrotesk-regular-webfont.585acda1.woff2"
+  },
+  {
+    "revision": "f2a4497d89f1f8d70707a35cb87acf06",
+    "url": "/Portfolio/static/media/hkgrotesk-regular-webfont.f2a4497d.woff"
+  },
+  {
+    "revision": "afb364ba5498aabfdb933a6b96f488cd",
+    "url": "/Portfolio/static/media/hkgrotesk-semibold-webfont.afb364ba.woff2"
+  },
+  {
+    "revision": "e9353041beea83b682b8ddfeceb88371",
+    "url": "/Portfolio/static/media/hkgrotesk-semibold-webfont.e9353041.woff"
+  }
+]);
