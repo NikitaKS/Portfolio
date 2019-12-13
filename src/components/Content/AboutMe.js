4b5f3d0aff0a47b1c20dfc3f0dfed787cd1b13ed
NikitaKS <pancxxx2@gmail.com>
2019-12-13 13:24:29 +0100

    AboutMeBlock

--- src/components/Content/AboutMe.js
+++ src/components/Content/AboutMe.js
@@ -1,51 +1,53 @@
-import React from 'react';
+import React, {Component} from 'react';
 import s from './Content.module.css';
 import '../../fonts/fonts.css';
+import AboutMeIn from "./AboutMeBlock/AboutMeIn";
+import Skills from "./AboutMeBlock/Skills";
 
-function AboutMe() {
-    return (
-        <div className={s.AboutMe}>
-            <div className={"container"}>
-                <div className={s.AboutMeWrapper}>
-                    <div className={s.AboutMeNav}>
-                        <ul className={s.NavMenu}>
-                            <li>About Me</li>
-                            <li>Skills</li>
-                            <li>Service</li>
-                            <li>Interview</li>
-                            <li>Award</li>
-                            <li>Ex</li>
-                        </ul>
-                    </div>
-                    <div className={s.AboutMeDescWrapper}>
-                        <div className={s.left}>
-                            <img src="https://spaceraceit.com/wp/growth/wp-content/uploads/2019/05/about-pic.png" alt=""/>
-                        </div>
-                        <div className={s.right}>
-                            <div className={s.title}>Why hire me for your next project?</div>
-                            <div className={s.desc}>
-                                <p>
-                                    Meet my startup design agency Shape Rex Currently I am working at CodeNext as Product
-                                    Designer. Before that I’ve worked at SpurBee, Dhaka as a UX, UI Designer.
-                                </p>
+class AboutMe extends Component {
+    state = {
+        buttonValue: ''
+    };
+    onClickButton = (e) => {
+        this.setState({
+            buttonValue: e.currentTarget.innerHTML
+        })
+    };
 
-                                <p>
-                                    I love to work in User Experience & User Interface designing. Because I love to solve
-                                    the
-                                    design problem and find easy and better solutions to solve it. I always try my best to
-                                    make
-                                    good user interface with the best user experience. I have been working as a Visual
-                                    designer
-                                    from.
-                                </p>
-                            </div>
+    render() {
+        if(this.state.buttonValue === 'Skills') {
+            return <Skills/>
+        }
+
+
+        return (
+            <div className={s.AboutMe}>
+                <div className={"container"}>
+                    <div className={s.AboutMeWrapper}>
+                        <div className={s.AboutMeNav}>
+                            <ul className={s.NavMenu}>
+                                <li onClick={this.onClickButton}>About Me</li>
+                                <li onClick={this.onClickButton}>Skills</li>
+                                <li onClick={this.onClickButton}>Service</li>
+                                <li onClick={this.onClickButton}>Interview</li>
+                                <li onClick={this.onClickButton}>Award</li>
+                                <li onClick={this.onClickButton}>Ex</li>
+                            </ul>
                         </div>
+
+                        <div className={s.AboutMeWrapper}>
+
+                            <AboutMeIn/>
+                            <Skills/>
+                        </div>
+
+
+
                     </div>
                 </div>
             </div>
-
-        </div>
-    );
+        );
+    }
 }
 
 export default AboutMe;
---
+++ src/components/Content/AboutMeBlock/Skills.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import s from '../Content.module.css';
+import '../../../fonts/fonts.css'
+
+class Skills extends Component {
+    render() {
+        return (
+            <div> SKILLLLSLLSLSLSLLSLSLS </div>
+        );
+    }
+}
+
+export default Skills;
